b10c4b9fa49cf1e68e6531a91e2590a4864d1cea
Anirudh <bhashyam.anirudh@gmail.com>
2026-09-14 16:49:59 +0530

    Add infinite looping.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#include "raylib-6.0_macos/include/raylib.h"
 #include "raylib.h"
 #include "raymath.h"
 
@@ -9,6 +8,10 @@
 #define WINDOW_HEIGHT 600
 #define WAVES_MAX_SIZE 2048
 #define TRAIL_MAX_SIZE 60
+
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
 
 // Assuming: RRGGBBAA
 #define HEX_TO_COLOR(x) (Color) { .r = ((x) >> 8 * 3) & 0xff, .g = ((x) >> 8 * 2) & 0xff, .b = ((x) >> 8 * 1) & 0xff, .a = ((x) >> 8 * 0) & 0xff }
@@ -31,8 +34,17 @@ typedef struct {
     float phase;
 } WaveForm;
 
+typedef struct {
+    Particle particle;
+    float time_elapsed;
+    float trail_time_elapsed;
+    float delay;
+    float trail_delay;
+} AppState;
+
 size_t trail_count = 0;
 static Particle trail[TRAIL_MAX_SIZE] = { 0 };
+
 
 void add_particle_to_trail(Particle particle) {
     // Acts like a ring buffer
@@ -74,6 +86,12 @@ void update_particle(Particle* particle, float dt) {
     particle->velocity = Vector2Add(particle->velocity, Vector2Scale(particle->acceleration, dt));
 }
 
+void update_waves(float dt) {
+    for (size_t i = 0; i < wave_count; ++i) {
+        waves[i].radius += waves[i].velocity * dt;
+    }
+}
+
 void emit_waveform(Particle* particle) {
     if (wave_count >= WAVES_MAX_SIZE) return;
     WaveForm wave = {
@@ -88,6 +106,47 @@ void emit_waveform(Particle* particle) {
 
 void draw_waveform(WaveForm* wave) {
     DrawCircleLinesV(wave->center, wave->radius + wave->phase, wave->color);
+}
+
+void reset_app_state(AppState* state) {
+    state->particle.position = (Vector2){ 0, WINDOW_HEIGHT / 2 };
+    state->particle.velocity = (Vector2){ 10.0f, 0.0f };
+    state->particle.acceleration = (Vector2){ 10.0f, 0.0f };
+
+    state->time_elapsed = 0.0f;
+    state->trail_time_elapsed = 0.0f;
+
+    wave_count = 0;
+    trail_count = 0;
+}
+
+void update_and_draw(void* arg) {
+    AppState* state = arg;
+    const float dt = GetFrameTime();
+    state->time_elapsed += dt;
+    state->trail_time_elapsed += dt;
+
+    update_particle(&state->particle, dt);
+    if (state->particle.position.x >= WINDOW_WIDTH + 50) reset_app_state(state);
+    if (state->time_elapsed >= state->delay) {
+        emit_waveform(&state->particle);
+        add_particle_to_trail(state->particle);
+        state->time_elapsed = 0;
+    }
+
+    if (state->trail_time_elapsed >= state->trail_delay) {
+        add_particle_to_trail(state->particle);
+        state->trail_time_elapsed = 0;
+    }
+    update_waves(dt);
+    BeginDrawing();
+        ClearBackground(BACKGROUND_COLOR);
+        render_trail(0.2f);
+        DrawCircleV(state->particle.position, state->particle.radius, state->particle.color);
+        for (size_t i = 0; i < wave_count; ++i) {
+            draw_waveform(&waves[i]);
+        }
+    EndDrawing();
 }
 
 int32_t main() {
@@ -110,39 +169,21 @@ int32_t main() {
         .color = HEX_TO_COLOR(0xfe166daa)
     };
 
+    static AppState state = { 0 };
+    state.particle = particle;
+    state.time_elapsed = 0.0f;
+    state.trail_time_elapsed = 0.0f;
+    state.delay = 0.1f;
+    state.trail_delay = 0.03f;
+
     SetTargetFPS(120);
-    float time_elapsed = 0.0f;
-    float trail_time_elapsed = 0.0f;
-    const float delay = 0.1f;
-    const float trail_delay = 0.03f;
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop_arg(update_and_draw, &state, 0, 1);
+#else
     while (!WindowShouldClose()) {
-        const float dt = GetFrameTime();
-        time_elapsed += dt;
-        trail_time_elapsed += dt;
-
-        update_particle(&particle, dt);
-        if (time_elapsed >= delay) {
-            emit_waveform(&particle);
-            add_particle_to_trail(particle);
-            time_elapsed = 0;
-        }
-
-        if (trail_time_elapsed >= trail_delay) {
-            add_particle_to_trail(particle);
-            trail_time_elapsed = 0;
-        }
-        for (size_t i = 0; i < wave_count; ++i) {
-            waves[i].radius += waves[i].velocity * dt;
-        }
-        BeginDrawing();
-            ClearBackground(BACKGROUND_COLOR);
-            render_trail(0.2f);
-            DrawCircleV(particle.position, particle.radius, particle.color);
-            for (size_t i = 0; i < wave_count; ++i) {
-                draw_waveform(&waves[i]);
-            }
-        EndDrawing();
+        update_and_draw(&state);
     }
+#endif
     CloseWindow();
     return 0;
 }
